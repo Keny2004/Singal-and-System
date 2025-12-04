fd91a4023c40b90aa03c8193f352ea9a8a87f27b
benny31610022-a11y <benny31610022@gmail.com>
2025-12-04 10:18:02 +0800

    Update dtmfdial.m

--- final/dtmfdial.m
+++ final/dtmfdial.m
@@ -18,17 +18,18 @@ dtmf.rowTones = [697,770,852,941];
 tt=0:1/fs:0.2; %每個字撥放0.2秒
 sil=zeros(1,fix(fs/100*2)); %中間間隔0.02秒
 % xx=zeros(1,length(tt)*length(keyNames));
-xx2=0;
+xx2=0; %輸出音的暫存器
 for kk=1:length(keyNames)
 [ii,jj] = find(keyNames(kk)==dtmf.keys); %找出每個字在dtmf.key的位置
 A=dtmf.colTones(jj); %高頻頻率
 B=dtmf.rowTones(ii); %低頻頻率
 amp=10; %輸出聲音大小
 
-xx1=amp*(cos(2*pi*A*tt)+cos(2*pi*B*tt)); %將找出的兩個頻率合成並撥放
-xx2=[xx2 sil xx1]; %播放靜音
+xx1=amp*(cos(2*pi*A*tt)+cos(2*pi*B*tt)); %將找出的兩個頻率合成
+xx2=[xx2 sil xx1]; %將合成音與間隔音加到xx2陣列
 end
 xx=xx2(2:end);
+
 
 
 
