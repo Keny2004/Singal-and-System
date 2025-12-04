d4073c5cdb5f4df16a76babb8d157bcb742f2434
benny31610022-a11y <benny31610022@gmail.com>
2025-12-04 09:58:49 +0800

    Update main.m

--- final/main.m
+++ final/main.m
@@ -4,8 +4,8 @@ string = 10;   %隨機產生10個按鍵
 
 %隨機產生字串
 char = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','*','#'];
-randomnum = randi(length(char), 1,string);
-tk = char(randomnum); %replace char
+randomnum = randi(length(char), 1,string); %randomnum陣列內容為char的index
+tk = char(randomnum); %將index轉為char陣列字元
 
 fprintf('\n');
 fprintf('隨機產生的字串: %s\n', tk);
@@ -22,4 +22,5 @@ if strcmp(tk, decoded)
     fprintf('Result = Correct\n');
 else
     fprintf('Result = Error\n');
+
 end
