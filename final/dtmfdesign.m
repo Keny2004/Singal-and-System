01964f9ea8723456c7c7df198e44c9f7407d4de0
Keny Huang <124441392+Keny2004@users.noreply.github.com>
2025-12-04 10:12:14 +0800

    Refactor dtmfdesign function comments and code
    Refactor dtmfdesign function to improve readability and maintainability.

--- final/dtmfdesign.m
+++ final/dtmfdesign.m
@@ -11,11 +11,11 @@ function  hh = dtmfdesign(fb, L, fs)
 %    maximum magnitude equal to one.
 
 %<=========================================FILL IN THE CODE HERE
-hh = zeros(L, length(fb));
-n = 0:L-1;
+n = 0:L-1;%建立0~L-1值的數列
 
-for i = 1:length(fb)
-% from PPT，b = 2/L 
-hh(:, i) = (2/L) * cos(2*pi*fb(i)*n/fs); 
+for i = 1:length(fb)%每個頻率的長度
+%b = 2/L 
+hh(:, i) = (2/L) * cos(2*pi*fb(i)*n/fs); %用for迴圈來把每個濾波器的值填入每行
 end
+
 end
