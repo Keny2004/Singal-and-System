e1d64752c1090cdeb1c0fd796d4b1d3a8d29a4db
Keny Huang <124441392+Keny2004@users.noreply.github.com>
2025-12-06 08:49:11 +0800

    更新 main.m 並新增註解

--- final/main.m
+++ final/main.m
@@ -10,22 +10,22 @@ len = floor((max_len-min_len).*rand(1,1) + min_len);%隨機產生字串長度
 r = floor((end_num-start_num).*rand(1,len) + start_num);%隨機產生字串內容
 tk = dict(r)%字串
 
-fprintf('\n');
+fprintf('-------------------------------------\n');
 fprintf('隨機產生的字串: %s\n', tk);
 
 %合成與播放
-xx = dtmfdial(tk, fs);
-soundsc(xx, fs);
+xx = dtmfdial(tk, fs);%呼叫把頻率合成的函式
+soundsc(xx, fs);%把合成出來的xx播放出來
 
 %解碼
 decoded = dtmfrun(xx, L, fs); 
 fprintf('系統解碼結果: %s\n', decoded);
 
 if strcmp(tk, decoded)
-    fprintf('Result = Correct\n');
+    fprintf('成功解碼\n');
 else
-    fprintf('Result = Error\n');
-
+    fprintf('解碼失敗\n');
 end
+fprintf('-------------------------------------\n');
 
 
